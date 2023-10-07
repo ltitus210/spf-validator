fac67ce08cf551d0a0ede08a756c988060a323a5
Frank Corso <frank@frankcorso.me>
2023-10-07 12:03:15 -0400

    Start creating checks for catchall piece of SPF

--- spf-validator.py
+++ spf-validator.py
@@ -15,7 +15,7 @@ def validate_spf_string(spf: str) -> bool:
     if not spf:
         return False
 
-    version_regex = re.compile(r"(?P<version>\bv=\S+\b)")
+    version_regex = re.compile(r"(\bv=\S+\b)")
     version_instances = version_regex.findall(spf)
 
     # First, make sure we are not missing the version instance.
@@ -28,5 +28,20 @@ def validate_spf_string(spf: str) -> bool:
 
     # Next, make sure the version instance is at the beginning of the string.
     if version_regex.match(spf).span()[0] != 0:
+        return False
+
+    catchall_regex = re.compile(r"(\S?all\b)")
+    catchall_instances = catchall_regex.findall(spf)
+
+    # Next, make sure we have at least 1 catchall instance.
+    if len(catchall_instances) == 0:
+        return False
+
+    # Next, make sure we only have 1 catchall instance.
+    if len(catchall_instances) > 1:
+        return False
+
+    # Next, make sure the catchall instance is at the end of the string.
+    if catchall_regex.search(spf).span()[1] != len(spf):
         return False
 
